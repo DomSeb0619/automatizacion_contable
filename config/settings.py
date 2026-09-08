17aa2ad8e334ed7808896672d27c671be33e13c6
robsebas <espinozaminango1991@gmail.com>
2026-09-08 16:53:03 -0500

    Configurar STATIC_ROOT para produccion

--- config/settings.py
+++ config/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 INSTITUTION_LOGO_MAX_SIZE = 2 * 1024 * 1024
